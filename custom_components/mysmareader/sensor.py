0d71fe4a0bc2c0b973ff8af8838ee6a1a9ec01c4
karelvdvd <karel.vdvd@gmail.com>
2026-06-02 15:01:18 +0200

    echte test
    echte test

--- custom_components/mysmareader/sensor.py
+++ custom_components/mysmareader/sensor.py
@@ -1,4 +1,12 @@
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import UnitOfPower
+
+from .const import CONF_HOST, CONF_PORT
+from .sma import read_current_power
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -6,14 +14,32 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     async_add_entities(
         [
-            MySMATestSensor(),
-        ]
+            MySMAPowerSensor(hass, entry),
+        ],
+        True,
     )
 
 
-class MySMATestSensor(SensorEntity):
-    """Test sensor."""
+class MySMAPowerSensor(SensorEntity):
+    """SMA current power sensor."""
 
-    _attr_name = "My SMA Test"
-    _attr_native_value = "OK"
-    _attr_unique_id = "mysma_test_sensor"
+    _attr_name = "My SMA Current Power"
+    _attr_unique_id = "mysma_current_power"
+    _attr_device_class = SensorDeviceClass.POWER
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_native_unit_of_measurement = UnitOfPower.WATT
+
+    def __init__(self, hass, entry):
+        self.hass = hass
+        self.host = entry.data[CONF_HOST]
+        self.port = entry.data[CONF_PORT]
+        self._attr_native_value = None
+
+    async def async_update(self):
+        """Update sensor value."""
+
+        self._attr_native_value = await read_current_power(
+            self.hass,
+            self.host,
+            self.port,
+        )
--- custom_components/mysmareader/sma.py
+++ custom_components/mysmareader/sma.py
@@ -1,5 +1,10 @@
 import socket
 
+from pymodbus.client import ModbusTcpClient
+
+
+SMA_UNIT_ID = 3
+
 
 async def test_connection(hass, host, port, timeout=5):
     """Test TCP connection to SMA inverter."""
@@ -12,3 +17,37 @@ async def test_connection(hass, host, port, timeout=5):
             return False
 
     return await hass.async_add_executor_job(_test)
+
+
+async def read_current_power(hass, host, port):
+    """Read current PV power from SMA inverter."""
+
+    def _read():
+        client = ModbusTcpClient(host=host, port=port, timeout=5)
+
+        try:
+            if not client.connect():
+                return None
+
+            result = client.read_holding_registers(
+                address=30775,
+                count=2,
+                slave=SMA_UNIT_ID,
+            )
+
+            if result.isError():
+                return None
+
+            registers = result.registers
+
+            value = (registers[0] << 16) + registers[1]
+
+            if value >= 0x80000000:
+                value -= 0x100000000
+
+            return value
+
+        finally:
+            client.close()
+
+    return await hass.async_add_executor_job(_read)
