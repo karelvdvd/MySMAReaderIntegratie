d446965f3a0eb20c742fbe7022f1a8cef6145631
karelvdvd <karel.vdvd@gmail.com>
2026-06-02 14:55:53 +0200

    Update sma.py

--- custom_components/mysmareader/sma.py
+++ custom_components/mysmareader/sma.py
@@ -1,2 +1,14 @@
-async def test_connection(host, port):
-    ...
+import socket
+
+
+async def test_connection(hass, host, port, timeout=5):
+    """Test TCP connection to SMA inverter."""
+
+    def _test():
+        try:
+            with socket.create_connection((host, port), timeout=timeout):
+                return True
+        except OSError:
+            return False
+
+    return await hass.async_add_executor_job(_test)
